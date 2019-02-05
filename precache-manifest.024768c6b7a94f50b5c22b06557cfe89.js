2fc02bb6d590fbaa46c07c38318edee2ecf8a28b
Jim Disenstam <jim.disenstam@gmail.com>
2019-02-05 13:31:25 +0100

    Updates

---
+++ precache-manifest.024768c6b7a94f50b5c22b06557cfe89.js
@@ -0,0 +1,127 @@
+self.__precacheManifest = [
+  {
+    "revision": "f12cb7dbd9b0b923dd5e",
+    "url": "style.affa48e36ad76360e298.css"
+  },
+  {
+    "url": "main.f12cb7dbd9b0b923dd5e.js"
+  },
+  {
+    "url": "dd1fdfda5d8484043f456d63b80a966f.txt"
+  },
+  {
+    "url": "2a917eda07425bc440b51803aec656c6.png"
+  },
+  {
+    "revision": "832a289768462166374dd10820f1b747",
+    "url": "index.html"
+  },
+  {
+    "revision": "2a917eda07425bc440b51803aec656c6",
+    "url": "image/cursor.png"
+  },
+  {
+    "revision": "13442edcb4452895ef75400600a80fbf",
+    "url": "image/0.png"
+  },
+  {
+    "revision": "4edfd5b3ec42748e3591647223cbc4c5",
+    "url": "image/button-max.png"
+  },
+  {
+    "revision": "36ab94bed5e0c23118fafd834e792dce",
+    "url": "image/favicon.png"
+  },
+  {
+    "revision": "b1a08114d4af347ae2f4d49d8723b354",
+    "url": "image/memory-icon.png"
+  },
+  {
+    "revision": "3f3987ec94cef752d383bd5c340a02d7",
+    "url": "image/twenty-one-icon.png"
+  },
+  {
+    "revision": "eafa13ff07071ff8d027797b78a30b6a",
+    "url": "image/button-min.png"
+  },
+  {
+    "revision": "1238ae50bbd31c641751ff856d5ba836",
+    "url": "image/10.png"
+  },
+  {
+    "revision": "a73e6efffbdc519691bbd7a7a9dbba5f",
+    "url": "image/15.png"
+  },
+  {
+    "revision": "b8e3ffe93f716cf9ae81d1849425c1ec",
+    "url": "image/18.png"
+  },
+  {
+    "revision": "39b6cae8c8df8112c987319bf53d3d63",
+    "url": "image/4.png"
+  },
+  {
+    "revision": "b0b02c8cc7f6f0e006964254620f16f0",
+    "url": "image/5.png"
+  },
+  {
+    "revision": "084fde76f873cfec0f9cb82610f5dc49",
+    "url": "image/6.png"
+  },
+  {
+    "revision": "32f64da02f7ebc18566aa80d378862ef",
+    "url": "image/9.png"
+  },
+  {
+    "revision": "bcc3e62f4556c34348f755b80a67f309",
+    "url": "image/11.png"
+  },
+  {
+    "revision": "f7be0b52283881f60eacd05dab1ca015",
+    "url": "image/12.png"
+  },
+  {
+    "revision": "d299d3424481295ef41ad78ac583f7bf",
+    "url": "image/13.png"
+  },
+  {
+    "revision": "2117412ef0324ca1be1d96ea8556686d",
+    "url": "image/16.png"
+  },
+  {
+    "revision": "842935435956bebabce425e7730f998e",
+    "url": "image/17.png"
+  },
+  {
+    "revision": "b95b3389f4708c569a427e2d3581b70a",
+    "url": "image/2.png"
+  },
+  {
+    "revision": "8af6b482aa8817a8836521b031abb03b",
+    "url": "image/7.png"
+  },
+  {
+    "revision": "d1f4bb52da74858aada6fa76246c9fb4",
+    "url": "image/8.png"
+  },
+  {
+    "revision": "b0e2bbc33af21fb8ed72fcf345424b42",
+    "url": "image/1.png"
+  },
+  {
+    "revision": "6332a00bedbcae391bd6ece97de77910",
+    "url": "image/14.png"
+  },
+  {
+    "revision": "b9abbba599cd5a28b0ddf79955569046",
+    "url": "image/3.png"
+  },
+  {
+    "revision": "84aa592f0d61014939feea2b7a5787a1",
+    "url": "image/twenty-one-bg.gif"
+  },
+  {
+    "revision": "f60bd7c6655364e621e9920ab85f8317",
+    "url": "image/favicon.ico"
+  }
+];
